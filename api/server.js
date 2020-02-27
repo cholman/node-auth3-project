8a0101c82c476b27048a14e477469e5494ee9433
cholman <holman.cody@gmail.com>
2020-02-27 14:40:31 -0600

    MVP

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
+const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router.js");
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/users', usersRouter);
+server.use('/api', authRouter);
 
 
 server.get('/', (req, res) => {
--- knexfile.js
+++ knexfile.js
@@ -4,48 +4,16 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './database/auth.db3'
-    },
     useNullAsDefault: true,
+    connection: {
+      filename: './database/auth.db3',
+    },
     migrations: {
-      directory: "./data/migrations"
+      directory: './database/migrations',
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: './database/seeds'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
